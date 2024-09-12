7d922981b0b3a4a8682b112d9c757b78cb1e3cb9
pratiksha karn <karnprtksh11@gmail.com>
2024-09-12 18:18:48 +1000

    feat: create home screen

--- App.js
+++ App.js
@@ -2,16 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import HomeScreen from "./screens/HomeScreen";
 
 // set up redux
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Uber Clone</Text>
-        <StatusBar style="auto" />
-      </View>
+      <HomeScreen />
     </Provider>
   );
 }
---
+++ screens/HomeScreen.js
@@ -0,0 +1,18 @@
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text style={styles.text}>HomeScreen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  text: {
+    color: "blue",
+  },
+});
