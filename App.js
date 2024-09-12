d0eb599f09a9b17bccf189400906e516f918c4e0
pratiksha karn <karnprtksh11@gmail.com>
2024-09-12 18:13:15 +1000

    feat: set up redux store

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 // set up redux
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Uber Clone</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Uber Clone</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
