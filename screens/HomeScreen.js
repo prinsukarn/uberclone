800dec89582030ae7656fb45c0a9d0feafd32b33
pratiksha karn <karnprtksh11@gmail.com>
2024-09-13 11:52:35 +1000

    feat: add dispatch, set origin and destination

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,16 +4,20 @@ import tw from "tailwind-react-native-classnames";
 import NavOptions from "../components/NavOptions";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import { GOOGLE_MAPS_API_KEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setDestination, setOrigin } from "../slices/navSlice";
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
+        {/* Logo */}
         <Image
           style={{ width: 100, height: 100, resizeMode: "contain" }}
           source={require("../assets/logo.png")}
         />
-
         {/* Google Search for places */}
         <GooglePlacesAutocomplete
           styles={{
@@ -24,14 +28,30 @@ const HomeScreen = () => {
               fontSize: 18,
             },
           }}
+          enablePoweredByContainer={false}
+          minLength={2}
           query={{
             key: GOOGLE_MAPS_API_KEY,
             language: "en",
           }}
+          onPress={(data, details = null) => {
+            dispatch(
+              setOrigin({
+                location: details.geometry.location,
+                description: data.description,
+              })
+            );
+
+            dispatch(setDestination(null));
+          }}
+          // search when return key pressed on keyboard
+          returnKeyType={"search"}
+          fetchDetails={true}
           placeholder="Where From?"
           nearbyPlacesAPI="GooglePlacesSearch"
           debounce={400}
         />
+        {/* Nav Options for Uber drive and food */}
         <NavOptions />
       </View>
     </SafeAreaView>
