1fdab7e99bb99069d231af431cab22847101237f
pratiksha karn <karnprtksh11@gmail.com>
2024-09-13 10:33:20 +1000

    feat: add navoptions, images

---
+++ components/NavOptions.js
@@ -0,0 +1,41 @@
+import { FlatList, Image, TouchableOpacity, View, Text } from "react-native";
+import React from "react";
+import tw from "tailwind-react-native-classnames";
+
+const data = [
+  {
+    id: "123",
+    title: "Get a ride",
+    image: require("../assets/UberX.webp"),
+    screen: "MapScreen",
+  },
+  {
+    id: "456",
+    title: "Order Food",
+    image: require("../assets/food.png"),
+    screen: "EatScreen",
+  },
+];
+
+const NavOptions = () => {
+  return (
+    <FlatList
+      data={data}
+      keyExtractor={(item) => item.id}
+      horizontal
+      renderItem={({ item }) => (
+        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+          <View>
+            <Image
+              style={{ width: 120, height: 120, resizeMode: "contain" }}
+              source={item.image}
+            />
+            <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+          </View>
+        </TouchableOpacity>
+      )}
+    />
+  );
+};
+
+export default NavOptions;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import { SafeAreaView, StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
 import tw from "tailwind-react-native-classnames";
+import NavOptions from "../components/NavOptions";
 
 const HomeScreen = () => {
   return (
@@ -10,6 +11,7 @@ const HomeScreen = () => {
           style={{ width: 100, height: 100, resizeMode: "contain" }}
           source={require("../assets/logo.png")}
         />
+        <NavOptions />
       </View>
     </SafeAreaView>
   );
