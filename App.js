a1409a96a1fba83b0d6d324761d96053e31634d3
pratiksha karn <karnprtksh11@gmail.com>
2024-09-13 11:05:43 +1000

    feat:add navigation, install react-navigation/native and stack

--- App.js
+++ App.js
@@ -1,18 +1,41 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import "react-native-gesture-handler";
+import { StyleSheet } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import { Provider } from "react-redux";
+
 import { store } from "./store";
 import HomeScreen from "./screens/HomeScreen";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import MapScreen from "./screens/MapScreen";
 
 // set up redux
 
 export default function App() {
+  const Stack = createStackNavigator();
+
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <HomeScreen />
-      </SafeAreaProvider>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="HomeScreen"
+              component={HomeScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="MapScreen"
+              component={MapScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
