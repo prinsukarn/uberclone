1d22214a8f0d8ce74f77b678222d2febc4538e05
pratiksha karn <karnprtksh11@gmail.com>
2024-09-13 10:44:21 +1000

    feat: add safearea context, aroow icon and padding

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,7 +6,7 @@ import NavOptions from "../components/NavOptions";
 const HomeScreen = () => {
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
-      <View>
+      <View style={tw`p-5`}>
         <Image
           style={{ width: 100, height: 100, resizeMode: "contain" }}
           source={require("../assets/logo.png")}
