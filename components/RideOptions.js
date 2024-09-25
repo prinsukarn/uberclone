a49fa1841bf1554292753d3478d29d3a7bc3b0fe
pratiksha karn <karnprtksh11@gmail.com>
2024-09-26 07:12:25 +1000

    feat: add chevron and title to ride options screen

--- components/RideOptions.js
+++ components/RideOptions.js
@@ -1,14 +1,30 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import tw from "tailwind-react-native-classnames";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { Icon } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
 
 const RideOptions = () => {
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>RideOptions</Text>
-    </View>
+    <SafeAreaView style={tw`bg-white flex-grow`}>
+      <View style={tw`flex-row`}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("NavigateCard")}
+          style={tw` rounded-full py-5`}
+        >
+          <Icon name="chevron-left" type="fontawesome" />
+        </TouchableOpacity>
+
+        <Text style={tw`text-center py-5 text-xl font-medium  w-5/6`}>
+          Select A Ride
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default RideOptions;
-
-const styles = StyleSheet.create({});
